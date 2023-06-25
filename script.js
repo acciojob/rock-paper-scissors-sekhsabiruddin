0a16a36058b35659a5072d5c8249d001996c4874
sk sabiruddin <120248500+sekhsabiruddin@users.noreply.github.com>
2023-06-25 07:43:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,80 +1,113 @@
-//your code here
-const compArray = ["rock", "paper", "scissor"];
-let userChoice = document.getElementById("user-choice").textContent;
-let computerChoice = document.getElementById("computer-choose").textContent;
-let userPoints = 0;
-document.getElementById("userPoints").textContent = userPoints;
-let compPoints = 0;
-document.getElementById("compPoints").textContent = compPoints;
+var gameNumberEl = document.getElementById("game-number");
+  var gameContainerEl = document.getElementById("game-container");
+  var rockBtnEl = document.getElementById("rock");
+  var paperBtnEl = document.getElementById("paper");
+  var scissorBtnEl = document.getElementById("scissors");
+  var computerChooseEl = document.getElementById("computer-choose");
+  var roundResultEl = document.getElementById("round-result");
+  var userPointsEl = document.getElementById("user-points");
+  var computerPointsEl = document.getElementById("computer-points");
+  var roundsLeftEl = document.getElementById("rounds-left");
+  var gameResultEl = document.getElementById("game-result");
+ 
 
-// game logic who wins and points increament
-function winnerWithChoice(userChoice, computerChoice) {
-  document.getElementById("user-choice").textContent = userChoice;
-  document.getElementById("computer-choose").textContent = computerChoice;
+  const ROCK = 0;
+  const PAPER = 1;
+  const SCISSORS = 2;
+ 
 
-  //  when match is tie
-  if (userChoice === computerChoice) {
-    document.getElementById("roundResult").textContent = "Tie";
+  function playGame() {
+  userPoints = 0;
+  userPointsEl.innerText = 0;
+  computerPoints = 0;
+  computerPointsEl.innerText = 0;
+ 
 
-    // when comp wins
-  } else if (
-    (userChoice === "rock" && computerChoice === "paper") ||
-    (userChoice === "paper" && computerChoice === "scissor") ||
-    (userChoice === "scissor" && computerChoice === "rock")
-  ) {
-    compPoints++;
-    document.getElementById("roundResult").textContent = "Computer";
-    document.getElementById("compPoints").textContent = compPoints;
-
-    // when user wins
+  roundsLeft = parseInt(gameNumberEl.value, 10);
+  if (roundsLeft < 0) {
+  alert("Number of turns cannot be -ve");
   } else {
-    userPoints++;
-    document.getElementById("roundResult").textContent = "User";
-    document.getElementById("userPoints").textContent = userPoints;
+  roundsLeftEl.innerText = roundsLeft;
+  gameContainerEl.style.display = "block";
+ 
+
+  gameResultEl.style.display = "none";
   }
+  }
+ 
 
-  // when user wins
-}
+  // game variables
+  var userPoints = 0;
+  var computerPoints = 0;
+  var roundsLeft = parseInt(gameNumberEl.value, 10);
+  roundsLeftEl.innerText = roundsLeft;
+ 
 
-// Game starts after clicking on play button
-document.getElementById("play-game").addEventListener("click", function () {
-  let noOfRounds = Number(document.querySelector("#game-number").value);
-  let roundsLeft = noOfRounds;
+ 
 
-  // for loop
-  //   for (let i = 1; i <= noOfRounds; i++) {
+  function userChoose(e) {
+ 
 
-  let randomNumber = Math.floor(Math.random() * 3);
-  roundsLeft--;
+  if (roundsLeft <= 0) return;
+ 
 
-  document.getElementById("rock").addEventListener("click", function () {
-    userChoice = "rock";
-    computerChoice = compArray[randomNumber];
-    winnerWithChoice(userChoice, computerChoice);
-  });
+  const choices = ["ROCK", "PAPER", "SCISSORS"];
+  const outcomes = ["WON", "TIE", "LOSE"];
+  const userChoose = parseInt(e.target.value, 10);
+  const computerChoose = Math.floor(Math.random() * 3);
+  window.computerChoose = computerChoose
+  console.log(window.computerChoose);
+  computerChooseEl.innerText = choices[computerChoose];
+  var outcome = 1; // [win, tie, loose]
+ 
 
-  document.getElementById("paper").addEventListener("click", function () {
-    userChoice = "paper";
-    document.getElementById("user-choice").textContent = userChoice;
-    computerChoice = compArray[randomNumber];
-    winnerWithChoice(userChoice, computerChoice);
-  });
+  if ((choices[computerChoose] == 'ROCK' && choices[userChoose] == 'PAPER') || (choices[computerChoose] == 'SCISSORS' && choices[userChoose] == 'ROCK') || (choices[computerChoose] == 'PAPER' && choices[userChoose] == 'SCISSORS')) {
+  // use Wins
+  outcome = 0;
+  userPoints += 1;
+  } else if ((choices[computerChoose] == 'PAPER' && choices[userChoose] == 'ROCK') || (choices[computerChoose] == 'ROCK' && choices[userChoose] == 'SCISSORS') || (choices[computerChoose] == 'SCISSORS' && choices[userChoose] == 'PAPER')) {
+  // lose
+  outcome = 2;
+  computerPoints += 1;
+  } else {
+  // tie
+  outcome = 1;
+  }
+ 
 
-  document.getElementById("scissor").addEventListener("click", function () {
-    userChoice = "scissor";
-    document.getElementById("user-choice").textContent = userChoice;
-    computerChoice = compArray[randomNumber];
-    winnerWithChoice(userChoice, computerChoice);
-  });
-  // updating the rounds left
-  document.getElementById("roundsLeft").textContent = roundsLeft;
+  // setting game variables after move
+  roundsLeft -= 1;
+ 
 
-  //   }
+ 
 
-  // Final Game winner
-  if (userPoints > compPoints) {
-    document.getElementById("game-result").textContent = "User";
-  } else if (userPoints < compPoints) {
-    document.getElementById("game-result").textContent = "Computer";
-  } else document.getElementById("game-result").textContent = "Tie";
-});
+  roundResultEl.innerText = `${outcomes[outcome]}`;
+  roundResultEl.style.display = "block";
+ 
+
+  userPointsEl.innerText = userPoints;
+  computerPointsEl.innerText = computerPoints;
+ 
+
+  roundsLeftEl.innerText = roundsLeft;
+ 
+
+  if (roundsLeft == 0) {
+  // game over
+  gameResultEl.innerText = `${outcomes[userPoints == computerPoints ? 1 : userPoints > computerPoints ? 0 : 2]}`;
+  gameResultEl.style.display = "block";
+ 
+
+  gameContainerEl.style.display = "none";
+ 
+
+  }
+ 
+
+  }
+ 
+
+  // button click
+  rockBtnEl.addEventListener('click', userChoose);
+  paperBtnEl.addEventListener('click', userChoose);
+  scissorBtnEl.addEventListener('click', userChoose)
